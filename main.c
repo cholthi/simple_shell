fbfed4c61bd062ea70213727ac7af73675e727a7
Chol Paul <cholthipaul@gmail.com>
2023-04-26 11:51:51 -0700

    Simple shell empty repo

---
+++ main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - A simple shell program
+ * @argc: int Argument count of the secnd array of strings
+ * @argv: Array Array of string arguments to the program
+ *
+ * Return: int 0 on success or any number above 0 on failure
+ */
+
+int main(argc int, argv **char)
+{
+	return (0);
+}
---
+++ main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * Desc: Declaration for types and function of the shell
+ *
+ */
+
+#endif
